ef3c3b275a8fb9e90cfda56b0f2759e3222be786
Jonah Temple <jdtempl2@users.noreply.github.com>
2022-06-13 12:51:02 -0400

    Added function to allocate cards for player stacks

--- src/Lib.hs
+++ src/Lib.hs
@@ -45,7 +45,10 @@ playerDeck :: PlayerCards -> Deck
 playerCutVal :: PlayerInfo -> Int
 playerNertz :: PlayerCards -> Stack
 playerHand :: PlayerCards -> Stack
---playerSoli :: PlayerCards -> [Stack]
+playerSoli1 :: PlayerCards -> Stack
+playerSoli2 :: PlayerCards -> Stack
+playerSoli3 :: PlayerCards -> Stack
+playerSoli4 :: PlayerCards -> Stack
 --updateDeck :: Player -> Deck -> Player
 createPlayer :: (Name, Int) -> Player
 
@@ -54,7 +57,10 @@ playerCutVal (PlayerInfo _ cutVal) = cutVal
 playerDeck (PlayerCards deck _ _ _ _ _ _) = deck
 playerNertz (PlayerCards _ nertzStack _ _ _ _ _) = nertzStack
 playerHand (PlayerCards _ _ handStack _ _ _ _) = handStack
---playerSoli (PlayerCards _ _ _ soliStack) = soliStack
+playerSoli1 (PlayerCards _ _ _ soli _ _ _) = soli
+playerSoli2 (PlayerCards _ _ _ _ soli _ _) = soli
+playerSoli3 (PlayerCards _ _ _ _ _ soli _) = soli
+playerSoli4 (PlayerCards _ _ _ _ _ _ soli) = soli
 
 playerInfo (Player info _) = info
 playerCards (Player _ cards) = cards
@@ -89,14 +95,34 @@ shuffle cutVal deck = shuffledDeck where
     shuffledDeck = drop cutVal almostShuffledDeck ++ take cutVal almostShuffledDeck -- cut the deck at 'cutVal' index 
 
 
+-- Take a subset of cards from a stack of Cards. Start and End are inclusive    
+takeCards :: (Int, Int) -> [Card] -> [Card]
+takeCards (start, end) cards = take (end-start+1) $ drop start cards
+
+
+setupPlayerStacks :: Player -> Player
+setupPlayerStacks player = player' where
+    pinfo = playerInfo player
+    pcards = playerCards player
+    deck' = shuffleNTimes 223 (playerCutVal pinfo) (playerDeck pcards)
+    nertz' = takeCards (0,12) deck'
+    soli1' = takeCards (13,13) deck'
+    soli2' = takeCards (14,14) deck'
+    soli3' = takeCards (15,15) deck'
+    soli4' = takeCards (16,16) deck'
+    hand' = takeCards (17,51) deck'
+    pcards' = PlayerCards deck' nertz' hand' soli1' soli2' soli3' soli4'
+    player' = Player pinfo pcards'
+
 playerNames = ["Alf", "Bob", "Cat", "Dog"]
 playerCutVals = [10, 20, 30, 40]
 playerInfos = zip playerNames playerCutVals
 
-players = map createPlayer playerInfos
+
+players = map (setupPlayerStacks . createPlayer) playerInfos
 
 
-jonah = createPlayer ("jonah", 17)
+jonah = (setupPlayerStacks . createPlayer) ("jonah", 17)
 jd = playerDeck $ playerCards jonah 
 jn = playerCutVal $ playerInfo jonah
 
