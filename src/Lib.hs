4fa7f4beb8e8ab0f2c802f0c64dac8739665b4c3
Jonah Temple <jdtempl2@users.noreply.github.com>
2022-06-13 09:19:51 -0400

    Rewrote the 'Player' structure

--- src/Lib.hs
+++ src/Lib.hs
@@ -33,33 +33,43 @@ removeTopCard :: Stack -> Stack
 removeTopCard = tail
 
 -- Player Object & methods
-data PlayerDeck = PlayerDeck Name Deck
-data Player = Player Name Int Deck Stack Stack [Stack] deriving (Show)
+data PlayerInfo = PlayerInfo Name Int deriving (Show)
+data PlayerCards = PlayerCards Deck Stack Stack Stack Stack Stack Stack deriving (Show)
+data Player = Player PlayerInfo PlayerCards deriving (Show)
 
-playerName :: Player -> Name
-playerDeck :: Player -> Deck
-playerCutVal :: Player -> Int
-playerNertz :: Player -> Stack
-playerHand :: Player -> Stack
-playerSoli :: Player -> [Stack]
-updateDeck :: Player -> Deck -> Player
-createPlayer :: Name -> Int -> Player
+playerCards :: Player -> PlayerCards
+playerInfo :: Player -> PlayerInfo
 
-playerName (Player name _ _ _ _ _) = name
-playerCutVal (Player _ cutVal _ _ _ _) = cutVal
-playerDeck (Player _ _ deck _ _ _) = deck
-playerNertz (Player _ _ _ nertzStack _ _) = nertzStack
-playerHand (Player _ _ _ _ handStack _) = handStack
-playerSoli (Player _ _ _ _ _ soliStack) = soliStack
+playerName :: PlayerInfo -> Name
+playerDeck :: PlayerCards -> Deck
+playerCutVal :: PlayerInfo -> Int
+playerNertz :: PlayerCards -> Stack
+playerHand :: PlayerCards -> Stack
+--playerSoli :: PlayerCards -> [Stack]
+--updateDeck :: Player -> Deck -> Player
+createPlayer :: (Name, Int) -> Player
 
-updateDeck player newDeck = Player (playerName player) (playerCutVal player) newDeck (playerNertz player) (playerHand player) (playerSoli player)
+playerName (PlayerInfo name _ ) = name
+playerCutVal (PlayerInfo _ cutVal) = cutVal
+playerDeck (PlayerCards deck _ _ _ _ _ _) = deck
+playerNertz (PlayerCards _ nertzStack _ _ _ _ _) = nertzStack
+playerHand (PlayerCards _ _ handStack _ _ _ _) = handStack
+--playerSoli (PlayerCards _ _ _ soliStack) = soliStack
+
+playerInfo (Player info _) = info
+playerCards (Player _ cards) = cards
+
+--updateDeck player newDeck = Player (playerName player) (playerCutVal player) newDeck (playerNertz player) (playerHand player) (playerSoli player)
 
 
-createPlayer name cutVal = Player name cutVal (map (setCardName name) (buildDeck 51)) [] [] [] where
-
+createPlayer (name, cutVal) = Player pinfo pcards where --PlayerInfo(name cutVal) PlayerCards(map (setCardName name) (buildDeck 51) [] [] [] [] [] []) where
+    pinfo = PlayerInfo name cutVal
+    pcards = PlayerCards (map (setCardName name) (buildDeck 51)) [] [] [] [] [] []
     setCardName name card = Card (cardSuit card) (cardValue card) name
     buildDeck 0 = [Card 0 1 ""]
-    buildDeck d = buildDeck (d-1) ++ [Card (div d 13) (mod d 13 + 1) ""]
+    buildDeck d = buildDeck (d-1) ++ [Card (div d 13) (mod d 13 + 1) ""]    
+--Player name cutVal (map (setCardName name) (buildDeck 51)) [] [] [] where
+
 
 transferCard :: (Stack, Stack) -> (Stack, Stack)
 transferCard (stackFrom, stackTo) = (tail stackFrom, head stackFrom:stackTo)
@@ -72,21 +82,21 @@ shuffleNTimes n cutVal deck = shuffleNTimes (n-1) cutVal shuffledDeck where
 
 shuffle :: Int -> [Card] -> [Card]
 shuffle cutVal deck = shuffledDeck where
-    halfLen = div (length deck) 2
-    sd1 = take halfLen deck
-    sd2 = drop halfLen deck
-    turnToList a b = [a,b]
-    almostShuffledDeck = concat (zipWith (\a b -> [a, b]) sd1 sd2) 
-    shuffledDeck = drop cutVal almostShuffledDeck ++ take cutVal almostShuffledDeck
+    halfLen = div (length deck) 2 -- half-way index of deck
+    sd1 = take halfLen deck -- 1st half of deck
+    sd2 = drop halfLen deck -- 2nd half of deck
+    almostShuffledDeck = concat (zipWith (\a b -> [a, b]) sd1 sd2) -- interleave 2 halves
+    shuffledDeck = drop cutVal almostShuffledDeck ++ take cutVal almostShuffledDeck -- cut the deck at 'cutVal' index 
 
 
 playerNames = ["Alf", "Bob", "Cat", "Dog"]
 playerCutVals = [10, 20, 30, 40]
+playerInfos = zip playerNames playerCutVals
+
+players = map createPlayer playerInfos
 
 
-players = map createPlayer playerNames
-
-jonah = createPlayer "jonah" 17
-jd = playerDeck jonah 
-jn = playerCutVal jonah
+jonah = createPlayer ("jonah", 17)
+jd = playerDeck $ playerCards jonah 
+jn = playerCutVal $ playerInfo jonah
 
